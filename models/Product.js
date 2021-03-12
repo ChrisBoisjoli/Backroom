a6581d7aa69d65b9cb1838da0ab23e2e8ba832f4
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-03-12 15:27:43 -0600

    update columns product.js

--- models/Product.js
+++ models/Product.js
@@ -10,47 +10,62 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
-
-//     Product
-
 // id
-
+id: {
+  type: DataTypes.INTEGER,
+  allowNull: false,
+  primaryKey: true,
+  autoIncrement: true,
+},
 // Integer.
-
 // Doesn't allow null values.
-
 // Set as primary key.
-
 // Uses auto increment.
 
 // product_name
-
+product_name: {
+  type: DataTypes.STRING,
+  allowNull: false,
+},
 // String.
-
 // Doesn't allow null values.
 
 // price
-
+  price: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+    validate: {
+      isDecimal: true,
+    },
+  },
 // Decimal.
-
 // Doesn't allow null values.
-
 // Validates that the value is a decimal.
 
 // stock
+stock: {
+  type: DataTypes.INTEGER,
+  allowNull: false,
+  defaultValue: 10,
+  validate: {
+    isNumeric: true,
+  }
+},
 
 // Integer.
-
 // Doesn't allow null values.
-
 // Set a default value of 10.
-
 // Validates that the value is numeric.
 
 // category_id
-
+category_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'category',
+      key: 'id',
+    },
+},
 // Integer.
-
 // References the Category model's id.
   },
   {
