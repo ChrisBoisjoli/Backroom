06f1c3d0b06c7bffd791403b81771f3c6892a4b8
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-03-12 15:02:13 -0600

    add id and name for category

--- models/Category.js
+++ models/Category.js
@@ -4,22 +4,31 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
-//Category
 
-//id
-//integer
-//notnull
-//primary key
-//autoincrement
-
-// category_name
-
-// String.
-
-// Doesn't allow null values.
 Category.init(
   {
     // define columns
+    //Category
+
+  //id
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  //integer
+  //notnull
+  //primary key
+  //autoincrement
+
+  // category_name
+  category_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  // String.
+  // Doesn't allow null values.
   },
   {
     sequelize,
