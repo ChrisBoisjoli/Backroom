285395511a9b1716372012e6ea82b7c69e954133
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-03-14 18:38:32 -0500

    created get tag by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -18,9 +18,19 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-  // try {
-  //   const tagData = await 
-  // }
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include:[{model: Product, through: ProductTag}],
+    });
+    if (!tagData) 
+    if (!tagData) {
+      res.status(404).json({ message: 'No Category found with that id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  }catch (err) {
+    res.status(500).json(err);
+  }
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
