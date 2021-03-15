3330daae7c476cae6b41a2eea75b085badb5e1e3
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-03-14 19:50:56 -0500

    updated through indes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,7 +7,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
 try {
   const productData = await Product.findAll({
-    include: [{model: Category}, {model: Tag, through: ProductTag}],
+    include: [{model: Category}, {model: Tag, through: ProductTag, as: 'product_tags'}],
   });
   res.status(200).json(productData);
 } catch (err) {
@@ -21,7 +21,7 @@ try {
 router.get('/:id', async (req, res) => {
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include:[{model: Category}, {model: Tag, through: ProductTag}],
+      include:[{model: Category}, {model: Tag, through: ProductTag, as: 'product_tags'}],
     });
     if (!productData) 
     if (!productData) {
