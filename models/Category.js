5c9528fa497bcc589ca4cc403539d003b2435c6e
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-03-14 19:15:43 -0500

    updated index with through logic

--- models/Category.js
+++ models/Category.js
@@ -7,28 +7,17 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
-    //Category
-
-  //id
   id: {
     type: DataTypes.INTEGER,
     allowNull: false,
     primaryKey: true,
     autoIncrement: true,
   },
-  //integer
-  //notnull
-  //primary key
-  //autoincrement
-
-  // category_name
   category_name: {
     type: DataTypes.STRING,
     allowNull: false,
-  },
-  // String.
-  // Doesn't allow null values.
+  }
+
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -17,21 +17,10 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag, 
-    unique:false
-  },
-  // as: 'tag_id'
-});
+Product.belongsToMany(Tag, { through: ProductTag })
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {through: {
-  model: ProductTag, 
-  unique:false
-},
-// as: 'product_id'
-});
+Tag.belongsToMany(Product, { through: ProductTag })
 
 module.exports = {
   Product,
